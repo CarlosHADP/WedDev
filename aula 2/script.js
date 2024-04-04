33e533cbd63aa1a8e9631a052e87866a750c1ea6
Carlos Henrique <carloshadp@gmail.com>
2024-04-03 22:42:29 -0300

    aula 2

---
+++ aula 2/script.js
@@ -0,0 +1,36 @@
+/* function media(x, y){
+   soma = x + y;
+    return console.log(soma / 2);
+}
+
+const be = 'Mauricets';
+var bebe = 'Mauricets';
+let bezin = 'Mauricets';
+
+// console.log(be,'\n',bebe,'\n',bezin)
+let n = prompt('Type any number: ');
+
+if (n != 0){
+    console.log(n)
+}else{
+    console.log('Ocê coloco um 0')
+} */
+
+
+/* var nome = 'Carlos';
+var nome2 = prompt('Digite seu nome: ');
+var idade = 18;
+
+alert('Hello World!');
+prompt('Something:');
+confirm('Are u sure?');
+console.log('Carlos');
+console.log(nome, idade);
+console.log(`Welcome ${nome} `) */
+
+var nome = prompt('Digite seu nome: ');
+var idade = prompt('Digite sua idade: ');
+var profissao = prompt('Digite sua profissão: ');
+
+console.log(`Olá ${nome}, você tem ${idade}, e trabalha com ${profissao}`);
+alert('Ola '+nome+', você tem'+idade+' anos, e trabalha com '+profissao);
