9a3b4ebac28269a567a65f39dbb4ecdb7615731d
Carlos Henrique Alves <carloshadp@gmail.com>
2024-04-11 01:54:44 -0300

    Calculadora cores feias FINALIZADA

--- aula 3 - calculator/script.js
+++ aula 3 - calculator/script.js
@@ -1,15 +1,37 @@
-function clicar(){
+
+function soma(){
     var n1 = parseFloat(document.getElementById('number1').value);
     var n2 = parseFloat(document.getElementById('number2').value);
-    var resultado = document.getElementById('resultado');
+    var resultadoSoma = document.getElementById('soma');
+    var somar = n1 + n2;
 
-
-
-
-
-    var soma = n1 + n2;
-    
-    resultado.innerText = soma;
+    resultadoSoma.innerText = somar;
 }
 
+function sub(){
+    var n1 = parseFloat(document.getElementById('number1').value);
+    var n2 = parseFloat(document.getElementById('number2').value);
+    var resultadoSub = document.getElementById('sub');
+    var subtrair = n1 / n2;
+    
+    resultadoSub.innerText = subtrair;
+}
 
+function mult(){
+    var n1 = parseFloat(document.getElementById('number1').value);
+    var n2 = parseFloat(document.getElementById('number2').value);
+    var resultadoMult = document.getElementById('mult');
+    var multiplicar = n1 * n2;
+
+    resultadoMult.innerText = multiplicar;
+}
+
+function div(){
+    var n1 = parseFloat(document.getElementById('number1').value);
+    var n2 = parseFloat(document.getElementById('number2').value);
+    var resultadoDiv = document.getElementById('div');
+    var dividir = n1 / n2; 
+    
+    resultadoDiv.innerText = dividir;
+
+}
