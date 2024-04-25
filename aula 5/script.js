cbe73a26c0dacbf7baec44d89575f5e124ff912c
Carlos Henrique <carloshadp@gmail.com>
2024-04-24 22:29:58 -0300

    Aula 5 Loops

---
+++ aula 5/script.js
@@ -0,0 +1,19 @@
+for(i=1;i<=10;i++){
+    console.log(i)
+}
+
+function tabuada(){
+    var n = parseInt(document.getElementById("input").value)
+    // for (i=0;i<=10;i++){
+    //     console.log(n*i)
+    // }
+    var i = 0 
+    while(i<=10){
+        console.log(n*i)
+        i++
+    }
+}
+
+for(i=100;i>=0;i--){
+    console.log(i)
+}
