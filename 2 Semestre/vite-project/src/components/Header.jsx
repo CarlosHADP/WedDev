91e6271c57959e7ff7d630cec6a3efcfc8e85f19
Carlos Henrique <carloshadp@gmail.com>
2024-08-14 22:47:00 -0300

    a

---
+++ 2 Semestre/vite-project/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+
+    const nome = "Xinglong"
+    return ( 
+        <>
+            <h1>Olá, {nome}</h1>
+            <p></p>
+        </>
+     );
+}
+
+export default Header;
