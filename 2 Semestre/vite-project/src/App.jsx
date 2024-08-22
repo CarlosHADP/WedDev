d60f4cce68ad33eef591f1c6bc793ce54018464d
dudatiemiak <rm554756@fiap.com.br>
2024-08-21 22:41:20 -0300

    jh

--- 2 Semestre/vite-project/src/App.jsx
+++ 2 Semestre/vite-project/src/App.jsx
@@ -1,10 +1,12 @@
-import Header from "./components/Header"
-
+import Header from "./components/Header";
+import {Outlet} from "react-router-dom";
 function App() {
 
   return (
     <>
     <Header/>
+    <Outlet/>
+    <Footer/>
     </>
   )
 }
--- 2 Semestre/vite-project/src/main.jsx
+++ 2 Semestre/vite-project/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react"
+import ReactDOOM from "react-dom/client"
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Sobre from "/1espx/WedDev/2 Semestre/vite-project/src/pages/Sobre.jsx"
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+{
+  path: "/",
+  element: <App/>,
+  children:[
+    {path: "sobre", element: <Sobre/>}
+  ]
+},
+{
+  path: "/sobre",
+  element: <Sobre/>
+}
+
+])
+
+
+
+ReactDOOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router}/>
+  </StrictMode>
 )
