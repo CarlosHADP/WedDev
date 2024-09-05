38bc11fe6ebb052a6f49b31a795dcdc2a7b9c71d
Carlos Henrique <carloshadp@gmail.com>
2024-09-04 22:40:04 -0300

    Commit

---
+++ 2 Semestre/Aula 4/react-css/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./componentes/Header"
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+
+      <p className="text-blue-800 bg-gray-800 ml-8">Paragrafo</p>
+    </>
+  )
+}
+
+export default App
---
+++ 2 Semestre/Aula 4/react-css/src/componentes/Header.jsx
@@ -0,0 +1,19 @@
+import style from "./header.module.css"
+
+function Header(){
+
+    return(
+        <>
+        
+        <h1 className={style.titulo}>
+            Header
+        </h1>
+        
+        
+        
+        
+        </>
+    )
+}
+
+export default Header   
